a81949df9c0c2ddde348591e086dd8ce50c9d532
saiadityan <saiadityan18@gmail.com>
2023-01-10 20:56:23 +0530

    My second update

---
+++ Aditya_P2_MVN/src/test/java/p4/ClassD.java
@@ -0,0 +1,19 @@
+package p4;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ClassD 
+{
+	  @Test
+	  public void tc009() 
+	  {
+		  Reporter.log("tc009 executed successfull", true);
+	  }
+	  
+	  @Test
+	  public void tc0010() 
+	  {
+		  Reporter.log("tc010 executed successfull", true);
+	  }
+}
