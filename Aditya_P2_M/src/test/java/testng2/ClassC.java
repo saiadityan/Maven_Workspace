6944abadcb7ff43face86ec571ca7d97f1753240
saiadityan <saiadityan18@gmail.com>
2023-01-24 07:08:25 +0530

    My latest update

---
+++ Aditya_P2_M/src/test/java/testng2/ClassC.java
@@ -0,0 +1,13 @@
+package testng2;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ClassC 
+{
+  @Test
+  public void finatest() 
+  {
+	  Reporter.log("finaltest executed");
+  }
+}
